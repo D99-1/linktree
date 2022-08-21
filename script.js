4edfa76b648140d1fa1a727fa22d79468cc53b8e
D99-1 <79247409+D99-1@users.noreply.github.com>
2022-08-21 20:56:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
 
-const values = urlParams.values();
-var docURL = window.location.href,
-// var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; 
-const url = new URL(docURL);
+var url_string = window.location.href,
+var url = new URL(url_string);
 var c = url.searchParams.get("c");
 console.log(c);
-
 
 
