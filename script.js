0bf7c93db453624047b4ca08384bce7d54fbf465
D99-1 <79247409+D99-1@users.noreply.github.com>
2022-08-21 20:40:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const values = urlParams.values();
-
-for (const value of values) {
-  console.log(value);
-}
+var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; 
+var url = new URL(url_string);
+var c = url.searchParams.get("c");
+console.log(c);
